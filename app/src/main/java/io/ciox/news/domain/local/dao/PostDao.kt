eaaf9b58a54a5dc390611ac26e75e48bf25cd68c
musyokamuasya <josephmuasya8@gmail.com>
2021-09-24 01:52:13 +0300

    Create @PostDao for post CRUD method

---
+++ app/src/main/java/io/ciox/news/domain/local/dao/PostDao.kt
@@ -0,0 +1,25 @@
+package io.ciox.news.domain.local.dao
+
+import androidx.room.*
+import io.ciox.news.domain.local.model.Post
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface PostDao {
+    @Query("SELECT * FROM post_table ")
+    suspend fun getAllPosts(): Flow<List<Post>>
+
+    @Query("SELECT * FROM post_table WHERE title LIKE :name")
+    suspend fun getPostByTitle (name:String): Flow<List<Post>>
+
+    @Delete
+    suspend fun deletePosts(vararg post:Post)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun createPost(vararg post:Post)
+
+    @Update
+    suspend fun updatePost(vararg post:Post)
+
+}
--- app/src/main/java/io/ciox/news/domain/local/model/Post.kt
+++ app/src/main/java/io/ciox/news/domain/local/model/Post.kt
@@ -1,4 +1,4 @@
-package io.ciox.news.domain.local
+package io.ciox.news.domain.local.model
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "post_table")
 data class Post(
-    @PrimaryKey val id: Int? = null,
+    @PrimaryKey(autoGenerate = true) val id: Int? = null,
     @ColumnInfo(name = "title") val title: String?,
     @ColumnInfo(name = "content") val content : String?
 )
